73c50b2b964e05961286438923e2946e5e3eb3e9
atleyvarghese <atleyvarghese@gmail.com>
2017-11-02 19:28:04 +0530

    Twitter!

---
+++ basics-12-twitter/twitter.py
@@ -0,0 +1,30 @@
+
+class A:
+	KEY = 'wjRsWCMI3lU3ypwYjQbeIfKef'
+	SECRET = '3xB5nyISIdk8MOp4kutpRe4wnAdn2NgTGBEcrGWEV6HMDbPhth'
+	TOKEN = '1495931731-exp5kBvqUTp7lZJLFtZ9NmtKd7QwUIBKyRZB2QN'
+	TOKEN_SECRET = 'ysLFG2vK5Pp6NUIs4CnV6rL7XWzxFJtNfQjfxCA8p1GXo'
+
+
+api_keys = A()
+
+
+
+from requests_oauthlib import OAuth1Session
+import sys
+import json
+twitter = OAuth1Session(api_keys.KEY,
+                        client_secret=api_keys.SECRET,
+                        resource_owner_key=api_keys.TOKEN,
+                        resource_owner_secret=api_keys.TOKEN_SECRET)
+
+hashtag = ", ".join(sys.argv[1:])
+
+r = twitter.post( 'https://stream.twitter.com/1.1/statuses/filter.json', data={ 'track': hashtag }, stream=True )
+
+for line in r.iter_lines():
+    if line:
+        j = json.loads(line.decode('utf-8'))
+        print('\n')
+        print(j['user']['name'], " ----  @%s" % j['user']['screen_name'])
+        print(j['text'])
