dc7898631fecce157dcca406ec773a970c689fa7
jerinisready <jerinisready@gmail.com>
2017-11-08 22:12:44 +0530

    FACEBOOK ADDITIONS

--- basics-15-server/server.py
+++ basics-15-server/server.py
@@ -1,53 +1,57 @@
+
 #!/usr/bin/env python
 import socket
 import re
 
-def head():
-	return """HTTP/1.0 200 OK
-		Content-Type: text/html
 
-		"""
+def template(id):
+    return """HTTP/1.0 200 OK
+            Content-Type: text/html
 
-def template():
-	return 	"""
-		<!DOCTYPE html>
- 		   <head>
-			<title>Success</title>
-		   </head>
-		   <body>
-			Successfully receieved the data!!
-			Provided id is  \"""" + angle + """\"
-		   </body>
-		</html>
-		"""
 
-def fb_json(fb_id):
-	pass
+
+            <!DOCTYPE html>
+              <head>
+                <title>Success</title>
+                   </head>
+                   <body>
+                    Successfully receieved the data!!
+                    Provided id is  \"""" + id + """\"
+                    </body>
+                </html>
+            """
+
 
 # ##############################################################
 
-if __name__ == '__main__':
-	# Standard socket stuff:
-	host = 'localhost' # do we need socket.gethostname() ?
-	port = 8085
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind((host, port))
-	sock.listen(1) # don't queue up any requests
 
-	# Loop forever, listening for requests:
-	while True:
-		csock, caddr = sock.accept()
-		print "Connection from: " + `caddr`
-		req = csock.recv(1024) # get the request, 1kB max
-		print req
-	    # Look in the first line of the request for a move command
-	    # A move command should be e.g. 'http://server/move?a=90'
-		match = re.match('GET /\?id=([\w]+)\sHTTP/1', req)
-		if match:
-			id_from_url = match.group(1)
-			csock.sendall(head()+template())
-			csock.close()
-		else:
-			print "Returning 404"
-			csock.sendall("HTTP/1.0 404 Not Found\r\n\n <h1>404 Page Not Found!</h1>")
-			csock.close()
+def main():
+    # Standard socket stuff:
+    host = 'localhost'  # do we need socket.gethostname() ?
+    port = 8085
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    sock.listen(1)  # don't queue up any requests
+
+    # Loop forever, listening for requests:
+    while True:
+        print "Waiting....."
+        csock, caddr = sock.accept()
+        print "Connection from: " + `caddr`
+        req = csock.recv(1024)  # get the request, 1kB max
+        print req
+        match = re.match('GET /\?id=([-\.\w\d]+)\sHTTP/1', req)
+        if match:
+            angle = match.group(1)
+            print "id =: " + angle + ""
+            op = template(angle)
+            csock.sendall(op)
+            csock.close()
+        else:
+            print "Returning 404"
+            csock.sendall("HTTP/1.0 404 Not Found\r\n\n 404 Page Not Found!")
+            csock.close()
+
+
+if __name__ == '__main__':
+    main()
